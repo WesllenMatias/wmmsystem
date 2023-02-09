0432e0951d31ed4361a5194da2d7b5702e697aaf
U-DESKTOP-GN54QIC\Wesllen <wesllen.maia2010@gmail.com>
2023-02-09 10:40:46 -0300

    Ajustes em urls e settings

--- wmmtech/urls.py
+++ wmmtech/urls.py
@@ -22,10 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
    #path('logout/', home)
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        documment_root=settings.MEDIA_ROOT,
-    )
+]
