dfe1c66b4725ecc5b87781074ca6ba02d33e88d9
U-DESKTOP-GN54QIC\Wesllen <wesllen.maia2010@gmail.com>
2023-02-09 10:55:08 -0300

    Ajuste no urls.py adicionando configurações static files

--- wmmtech/urls.py
+++ wmmtech/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from orcamentos.views import home
 from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
